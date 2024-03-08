59f4fb029366bae72dce22dddc5814dc6cf7f025
trilokicode <162664933+trilokicode@users.noreply.github.com>
2024-03-08 08:40:54 +0000

    Strings

---
+++ basics/string.js
@@ -0,0 +1,36 @@
+/*
+const str = "triloki";
+const str2 = "kumar";
+const str3 = str+str2;
+console.log(str3);
+*/
+/*
+const str1 = "Triloki";
+const projectsDone = 45;
+console.log(`The name of student is ${str1} and projectsDone is ${projectsDone}`);
+
+const gameName = new String("Triloki");
+console.log(gameName);
+
+console.log(gameName.length);
+console.log(gameName.indexOf('i'));
+
+*/
+
+
+// Trim method
+/*
+let st = "        Triloki";
+console.log(st);
+console.log(st.trim());
+
+*/
+
+// Slice method
+
+let str = "Triloki";
+console.log(str.slice(2,4)) // 4 not included
+
+// replace();
+
+// includes();
