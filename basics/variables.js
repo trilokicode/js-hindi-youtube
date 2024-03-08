c5e08486fbb78a4a78bfbf183d620a73db8a312d
trilokicode <162664933+trilokicode@users.noreply.github.com>
2024-03-08 05:18:51 +0000

    Variables in JS

---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 12345;
+let accountName = "trilokiindiawale";
+var accountEmail = "triloki@google.com";
+accountCity = "Delhi"; // not a good way tpo declare a variable 
+
+console.log(accountId);
+//accountId = 12; // Can't assign a new value to the const variable
+console.log(accountId);
+
+console.log(accountName);
+console.table([accountId,accountName,accountCity]);
+
+/*
+    The var keyword should be avoided because of its functional scope
+*/
