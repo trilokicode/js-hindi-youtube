ad5ba2ddd5cc1ab45790467d944c5c876175194b
trilokicode <162664933+trilokicode@users.noreply.github.com>
2024-03-08 05:41:46 +0000

    Datatypes

---
+++ basics/datatype.js
@@ -0,0 +1,17 @@
+"use strict" // treat all codes as a newer version
+
+// alert(3+3); // we are using nodejs not browsers
+
+// datatypes in js
+/*
+    string
+    number
+    boolean
+    null
+    undefined
+    bigInt
+*/
+
+// null is an object
+
+
