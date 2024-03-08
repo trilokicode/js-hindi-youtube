afad761db2bd10e191c68afbb4991e15562f5830
trilokicode <162664933+trilokicode@users.noreply.github.com>
2024-03-08 09:28:40 +0000

    Numbers and Maths

---
+++ basics/NumbersMaths.js
@@ -0,0 +1,34 @@
+const score = 500;
+console.log(score);
+
+const score1 = new String("Triloki");
+console.log(score1);
+console.log(typeof score);
+console.log(typeof score1);
+
+// toPrecision function
+// toFixed()
+
+//Number.MAX_VALUE , Number.MIN_VALUE // max value of an integer
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//MATHS
+/*
+console.log(Math);
+console.log(Math.floor(5.6));
+console.log(Math.ceil(6.9));
+console.log(Math.round(4.8));
+console.log(Math.min(2,4,0,1,45));
+console.log(Math.max(123,45,678,890));
+
+*/
+
+// Random Number
+/*
+const min = 10 , max = 20 ;
+const num = Math.floor(Math.random() * (max-min+1) ) + min;
+console.log(num);
+
+*/
