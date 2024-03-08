136b610e8c9a183680e6fc9a5a247941b8a5b7f7
trilokicode <162664933+trilokicode@users.noreply.github.com>
2024-03-08 08:15:14 +0000

    DataTypeSummary

---
+++ basics/datatypeSummary.js
@@ -0,0 +1,33 @@
+// primitive
+
+// There are approax 7 types of primitive data types 
+/*
+DataType     Return Type
+
+String --->>  string
+Number --->>  number
+BigInt --->>  Function
+null   --->>  object
+undefined ->> undefined
+boolean -->>  boolean
+symbol --->>  Symbol
+*/
+let str = "Triloki";
+let num = 23;
+let isValid = true;
+const id = Symbol('123');
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof BigInt);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof isValid);
+console.log(typeof id);
+
+
+// Non Primitive --- >>>  Arrays, Objects , 
+
+
+// Stack (primitive datatype)  and Heap(Non-Primitive)
+
+//stack ke andar copy create hota hai. Heap ke andar reference.
