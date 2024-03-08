033fbb4319c33b8e493e048be350fa69de567a83
trilokicode <162664933+trilokicode@users.noreply.github.com>
2024-03-08 06:20:44 +0000

    Conversion

---
+++ basics/conversion.js
@@ -0,0 +1,29 @@
+/*let score = "33ab";
+
+console.log(score);
+console.log(typeof score);
+let newScore = Number(score);
+console.log(newScore);
+*/
+
+
+/*
+"33" ---> 33
+"33abc" ---> NaN
+true = 1 , false = 1;
+*/
+/*
+
+let str1 = "Triloki";
+let str2 = " Kumar";
+console.log(str1+str2);
+console.log(2*3);
+
+console.log("1" + 2);
+console.log(2 + "2");
+console.log("2" + 3 + 4);
+console.log(1 + 2 + 3 + "3");
+
+*/
+
+console.log(+true)// it is possible to write
