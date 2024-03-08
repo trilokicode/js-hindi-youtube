4dfdffba4cf4811133ce59d1c3da997c171c8659
trilokicode <162664933+trilokicode@users.noreply.github.com>
2024-03-08 06:35:17 +0000

    Comparison

---
+++ basics/comparison.js
@@ -0,0 +1,27 @@
+/*
+console.log(2>3);
+console.log(2>=3);
+console.log(2<3);
+console.log(2<=3);
+console.log(2==3);
+console.log(2!=3);
+*/
+
+// The below code should be avoided........
+
+/*
+console.log(null == 0);
+console.log(undefined > 0);
+console.log(null >= 0);
+*/
+
+/*
+console.log(undefined > 0);
+console.log(undefined >= 0);
+console.log(undefined == 0);
+
+*/
+
+// === it checks the value as well as datatype
+
+console.log("2" === 2) ;
