55038808e8fc9cf444ada9b6171f8dbaa27c381f
trilokicode <162664933+trilokicode@users.noreply.github.com>
2024-03-08 10:37:44 +0000

    Date

---
+++ basics/date.js
@@ -0,0 +1,44 @@
+
+
+/*
+const date = new Date();
+console.log(date.toString());
+console.log(date.toDateString());
+console.log(date.toLocaleString());
+console.log(typeof date);
+
+*/
+/*
+const myCreatedDate = new Date(2023 , 0 , 24 );
+
+console.log(myCreatedDate.toDateString());
+*/
+
+/*
+const myCreatedDate = new Date(2023 , 0 , 24 ,3,5 );
+console.log(myCreatedDate.toLocaleString());
+
+*/
+/*
+const myCreatedDate = new Date("01-14-2023");
+console.log(myCreatedDate.toLocaleString());
+
+*/
+/*
+const myCreatedDate = new Date("01-14-2023");
+let date = Date.now();
+//console.log(date);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000)); // second me dikhayega
+
+*/
+
+/*
+const date = new Date();
+console.log(date.toDateString());
+console.log(date.toLocaleString());
+console.log(date.toISOString());
+console.log(date.toTimeString());
+
+*/
+
